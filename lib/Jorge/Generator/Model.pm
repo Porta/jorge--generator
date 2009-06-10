5e414a39a9b467bed5564515f52ddadb915d59b2
Julián Porta <julian.porta@gmail.com>
2009-06-10 11:27:34 -0300

    plural, and sql generation

--- lib/Jorge/Generator/Model.pm
+++ lib/Jorge/Generator/Model.pm
@@ -29,13 +29,30 @@ use Carp qw( croak cluck);
 my @types = qw(
   bit
   char
+  datetime
   enum
   int
   text
   timestamp
-  datetime
   varchar
   pk
+);
+
+our $char_lenght    = 255;
+our $varchar_lenght = 255;
+our $int_lenght = 11;
+
+
+my %sql_types = (
+    bit         => 'TINYINT(1) NOT NULL',
+    char        => "CHAR($char_lenght) NOT NULL",
+    date        => 'DATETIME NOT NULL',
+    enum        => 'ENUM("") NOT NULL',
+    int       => "INT($int_lenght) NOT NULL",
+    pk          => "INT($int_lenght)  NOT NULL AUTO_INCREMENT",
+    text        => 'TEXT NOT NULL',
+    timestamp   => 'TIMESTAMP  NOT NULL default CURRENT_TIMESTAMP',
+    varchar     => "VARCHAR($char_lenght) NOT NULL",
 );
 
 sub run {
@@ -52,7 +69,7 @@ sub run {
     pod2usage() unless ( $config{_model} && $config{_fields} );
 
     $config{_model}  = ucfirst( $config{_model} );
-    $config{_plural} = ucfirst( $config{_plural} );
+    $config{_plural} = ucfirst( $config{_plural} ||  $config{_model} . 's');
 
     #now, parse the fields we received.
     my @fields = split( /,/, $config{_fields} );
@@ -75,6 +92,7 @@ sub run {
         push( @{ $config{_order} }, $field );
     }    
     singular(%config);
+    plural(%config);
 
 }
 
@@ -90,10 +108,14 @@ sub singular {
     #note the spacing. must match the number of spaces in the $tmpl var in
     # order to properly allign the fields
     my @pks = grep { $config{$_} eq 'Pk' } @fields;
-    my @datetimes       = grep { $config{$_} eq 'Timestamp' ||  $config{$_} eq 'Datetime' } @fields;
     my $pks_line        = join("\n        ", map { "$_ => { pk => 1, read_only => 1 }," } @pks);
+    my @datetimes       = grep { $config{$_} eq 'Timestamp' ||  $config{$_} eq 'Datetime' } @fields;
     my $datetime_line   = join("\n        ", map { "$_ => { datetime => 1 }," } @datetimes);
     my $classes_line    = join("\n        ", map { "$_ => { class => new $_() }," } @use);
+    
+    #sql code
+    my $sql_pks = join(",",@pks);
+    my $sql_fields = join("\n    ", map { "`$_` ". ( $sql_types{lc($config{$_})} || 'INT('.$int_lenght.') NOT NULL ') ." ," } @fields);
     
     
     my $tmpl = <<END;
@@ -125,8 +147,35 @@ sub _fields {
 }
 
 1;
+__END__
+CREATE TABLE IF NOT EXISTS `$config{_model}` (
+    $sql_fields
+    PRIMARY KEY  (`$sql_pks`)
+) ENGINE=MyISAM DEFAULT CHARSET=cp850 AUTO_INCREMENT=1 ;
+
 END
 
+    print $tmpl;
+}
+
+
+
+sub plural{
+    my %config = @_;
+    my $tmpl = <<END;
+package $config{_plural};
+use base 'Jorge::Collection';
+
+use $config{_model};
+
+use strict;
+
+sub create_object {
+	return new $config{_model};
+}
+
+1;
+END
     print $tmpl;
 }
 
